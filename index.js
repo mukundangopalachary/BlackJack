3a1204cec12cde056f4a201664e64c641470229a
mukundangopalachary <mukundangopalachary@gmail.com>
2024-06-01 22:43:24 +0530

    new feature 'player: $'

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ let secondCard = Math.floor(Math.random() * 13) + 1;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
-let new_cards = [];
-
 let msg = "";
-
+let new_cards = [];
+let player = {
+    name: "Per",
+    chips: 200
+}
+let playerEl = document.getElementById("player-el");
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardEl = document.getElementById("cards-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function startGame(){
     if (sum<21){
@@ -33,21 +38,6 @@ function newCard(){
         let card = Math.floor(Math.random() * 13) + 1;
         new_cards.push(card);
         sum += card;
-        if (sum<21){
-            msg = "Press 'New Card' to draw a new card! 🃏"
-
-        } else if (sum === 21){
-            msg = "Wohoo! You've got Blackjack! 🥳"
-            hasBlackJack = true;
-        } else {
-            msg = "You're out of the game! 😭"
-            isAlive = false;
-        }
-
-        messageEl.textContent = msg;
-       
-        sumEl.textContent = "Sum: " + sum;
-       
-        cardEl.textContent = "Cards: " + firstCard + ", " + secondCard + ", " + new_cards;
-    }    
+        startGame(); 
+}
 }
