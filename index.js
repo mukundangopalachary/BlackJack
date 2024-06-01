b8f17899451c6ef512ade7bdf3d2b88d678f7b19
mukundangopalachary <mukundangopalachary@gmail.com>
2024-06-01 19:09:54 +0530

    create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+let firstCard = Math.floor(Math.random() * 13) + 1;
+let secondCard = Math.floor(Math.random() * 13) + 1;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let new_cards = [];
+
+let msg = "";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("cards-el");
+
+function startGame(){
+    if (sum<21){
+        msg = "Press 'New Card' to draw a new card! 🃏"
+        
+    } else if (sum === 21){
+        msg = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true;
+    } else {
+        msg = "You're out of the game! 😭"
+        isAlive = false;
+    }
+
+    messageEl.textContent = msg;
+    sumEl.textContent = "Sum: " + sum;
+    cardEl.textContent = "Cards: " + firstCard + ", " + secondCard + ", " + new_cards;
+}
+
+function newCard(){
+    let card = Math.floor(Math.random() * 13) + 1;
+    new_cards.push(card);
+    sum += card;
+    if (sum<21){
+        msg = "Press 'New Card' to draw a new card! 🃏"
+        
+    } else if (sum === 21){
+        msg = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true;
+    } else {
+        msg = "You're out of the game! 😭"
+        isAlive = false;
+    }
+
+    messageEl.textContent = msg;
+    sumEl.textContent = "Sum: " + sum;
+    cardEl.textContent = "Cards: " + firstCard + ", " + secondCard + ", " + new_cards;
+}
